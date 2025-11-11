aab22dd1d92dd3bf7372ebbed7274613a5e7d3a9
aliyilmaz <ali.yilmaz.199920@icloud.com>
2025-11-11 17:26:58 +0300

    SaveChange Interceptor ChangeTracker

--- Presentation/ETradeAPI.WebAPI/Controllers/ProductsController.cs
+++ Presentation/ETradeAPI.WebAPI/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using ETradeAPI.Application.Repositories.ProductRepositories;
+﻿using ETradeAPI.Application.Repositories.OrderRepositories;
+using ETradeAPI.Application.Repositories.ProductRepositories;
 using ETradeAPI.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -12,10 +13,14 @@ namespace ETradeAPI.WebAPI.Controllers
         private readonly IProductReadRepository _productReadRepository;
         private readonly IProductWriteRepository _productWriteRepository;
 
-        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        private readonly IOrderWriteRepository _orderWriteRepository;
+        private readonly IOrderReadRepository _orderReadRepository;
+        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository, IOrderWriteRepository orderWriteRepository, IOrderReadRepository orderReadRepository)
         {
             _productReadRepository = productReadRepository;
             _productWriteRepository = productWriteRepository;
+            _orderWriteRepository = orderWriteRepository;
+            _orderReadRepository = orderReadRepository;
         }
 
         [HttpGet("get-products")]
@@ -35,6 +40,20 @@ namespace ETradeAPI.WebAPI.Controllers
         {
             await _productWriteRepository.CreateAsync(new Product { Id = Guid.NewGuid(), CreatedDate = DateTime.UtcNow, Name = "p1", Price = 100, Stock = 190 });
             await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+        [HttpGet("get-order")]
+        public async Task<IActionResult> GetOrder()
+        {
+            //await _orderWriteRepository.CreateAsync(new()
+            //{
+            //    Address = "Test",
+            //    Description = "Deneme"
+            //});
+            var value = await _orderReadRepository.GetByIdAsync("019a7345-1565-7a62-8a22-29040a001feb");
+            value.Address = "....Test";
+            value.Description = "aaaa";
+            await _orderWriteRepository.SaveAsync();
             return Ok();
         }
     }
