c74cb90fea943eb783102ef1c3a337c0ee2e197f
aliyilmaz <ali.yilmaz.199920@icloud.com>
2025-11-14 16:12:55 +0300

    Pagination

--- Presentation/ETradeAPI.WebAPI/Controllers/ProductsController.cs
+++ Presentation/ETradeAPI.WebAPI/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using ETradeAPI.Application.Features.Commands.Product.RemoveProduct;
 using ETradeAPI.Application.Features.Commands.Product.UpdateProduct;
 using ETradeAPI.Application.Features.Queries.Product.GetByIdProduct;
 using ETradeAPI.Application.Features.Queries.Product.GetProducts;
+using ETradeAPI.Application.Repositories.ProductRepositories;
 using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -15,25 +16,31 @@ namespace ETradeAPI.WebAPI.Controllers
     {
         private readonly IValidator<CreateProductCommandRequest> _createProductValidator;
         private readonly IMediator _mediator;
+        private readonly IProductReadRepository _productReadRepository;
 
-        public ProductsController(IValidator<CreateProductCommandRequest> createProductValidator, IMediator mediator)
+        public ProductsController(IValidator<CreateProductCommandRequest> createProductValidator, IMediator mediator, IProductReadRepository productReadRepository)
         {
             _createProductValidator = createProductValidator;
             _mediator = mediator;
+            _productReadRepository = productReadRepository;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetProducts([FromQuery] GetProductsQueryRequest request)
         {
+            var count = _productReadRepository.GetAll(false).Count();
             var values = await _mediator.Send(request);
-
-            return Ok(values);
+            return Ok(new
+            {
+                count,
+                Products = values
+            });
         }
         [HttpGet("{Id}")]
         public async Task<IActionResult> GetProduct([FromRoute] GetByIdProductQueryRequest request)
         {
             var value = await _mediator.Send(request);
-            if(value == null)
+            if (value == null)
                 return NotFound();
             return Ok(value);
         }
